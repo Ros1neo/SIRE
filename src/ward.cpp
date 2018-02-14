c334102680d01ff812e2e80e122d6e7606f0b29e
Luu Romain <romain.luu@gmail.com>
2018-02-14 09:58:35 +0100

    TD3 terminé

--- src/ward.cpp
+++ src/ward.cpp
@@ -50,20 +50,34 @@ std::string Ward::toString() const
 Vector3f Ward::sample_IS(const Vector3f inDir, const Vector3f normal, float *pdf) const{
     Vector3f i;
     do{
+
+    double randDiffSpec= (double)rand()/RAND_MAX;
+    double probDiffSpec =  m_specularColor.getLuminance() / (m_specularColor.getLuminance() + m_diffuseColor.getLuminance());
+    Vector3f X = (Vector3f(0,1.,0)-Vector3f(0,1.,0).dot(normal)*normal).normalized();
+    Vector3f Y = normal.cross(X).normalized();
     double u = (double)rand()/RAND_MAX;
     double v = (double)rand()/RAND_MAX;
 
+    if (randDiffSpec<probDiffSpec){
     double phi = atan2(m_alphaY*sin(2*M_PI*v),m_alphaX*cos(2*M_PI*v));
     double theta = atan(sqrt(-log(u)/(cos(phi)*cos(phi)/m_alphaX/m_alphaX+sin(phi)*sin(phi)/m_alphaY/m_alphaY)));
-
-    Vector3f X = (Vector3f(0,1.,0)-Vector3f(0,1.,0).dot(normal)*normal).normalized();
-    Vector3f Y = normal.cross(X).normalized();
 
     Vector3f h = (cos(phi)*sin(theta)*X+sin(phi)*sin(theta)*Y+cos(theta)*normal).normalized();
 
     double denom = 1.0f/4/M_PI/m_alphaX/m_alphaY/h.dot(inDir)/cos(theta)/cos(theta)/cos(theta);
     *pdf = denom*exp(-tan(theta)*tan(theta)*(cos(phi)*cos(phi)/m_alphaX/m_alphaX+sin(phi)*sin(phi)/m_alphaY/m_alphaY));
     i = (2*h.dot(inDir)*h-inDir).normalized();
+    }
+    else{
+        double phi =  2. * M_PI * u;
+
+        double z = 1.-v;
+        double sin_theta = sqrt(v*(2.-v));
+        double x = cos(phi)*sin_theta;
+        double y = sin(phi)*sin_theta;
+        *pdf = 1.0f/2.0f/M_PI;
+        i = (x*X+y*Y+z*normal).normalized();
+    }
 
     }while (i.dot(normal)<0);
     return i;
